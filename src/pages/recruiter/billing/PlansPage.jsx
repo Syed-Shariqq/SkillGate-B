3c772a279fbab8ba89371e35678a439dc81f6130
Syed Shariq <25cs1940@amceducation.in>
2026-06-14 19:21:55 +0530

    Fix: Undefined error

--- src/pages/recruiter/billing/PlansPage.jsx
+++ src/pages/recruiter/billing/PlansPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../../../context/AuthContext";
 import { supabase } from "../../../config/supabase";
